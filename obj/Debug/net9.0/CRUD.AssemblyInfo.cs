7de630d4be0c5d42bdcc90842d5cd1b5f8015593
sebaz07 <sebastian-martinez-07@hotmail.com>
2025-07-05 09:01:24 -0500

    actualización de pipeline

--- obj/Debug/net9.0/CRUD.AssemblyInfo.cs
+++ obj/Debug/net9.0/CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a67f4530e9b2b9600354685ba07f6ae3bd27b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
